6794c8c313cf07b51e58c80cf2d1020b7dd061fb
Curipoma <at.curipoma@yavirac.edu.ec>
2022-09-29 10:23:31 -0500

    added support for small characters

--- src/modules/core/dto/asset-details/create-asset-details.dto.ts
+++ src/modules/core/dto/asset-details/create-asset-details.dto.ts
@@ -6,35 +6,35 @@ export class CreateAssetDetailsDto {
   readonly asset: AssetEntity;
 
   @IsString({ message: 'annualExistence must be a string' })
-  @MinLength(3, {
+  @MinLength(1, {
     message: 'annualExistence, El número de caracteres mínimo es 3.',
   })
   @MaxLength(255, { message: 'annualExistence, Maximo 255 caracteres' })
   readonly annualExistence: string;
 
   @IsString({ message: 'code must be a string' })
-  @MinLength(3, {
+  @MinLength(1, {
     message: 'code, El número de caracteres mínimo es 3.',
   })
   @MaxLength(255, { message: 'code, Maximo 255 caracteres' })
   readonly code: string;
 
   @IsString({ message: 'initialExistence must be a string' })
-  @MinLength(3, {
+  @MinLength(1, {
     message: 'initialExistence, El número de caracteres mínimo es 3.',
   })
   @MaxLength(255, { message: 'initialExistence, Maximo 255 caracteres' })
   readonly initialExistence: string;
 
   @IsString({ message: 'unitValue must be a string' })
-  @MinLength(3, {
+  @MinLength(1, {
     message: 'unitValue, El número de caracteres mínimo es 3.',
   })
   @MaxLength(255, { message: 'unitValue, Maximo 255 caracteres' })
   readonly unitValue: string;
 
   @IsString({ message: 'value must be a string' })
-  @MinLength(3, {
+  @MinLength(1, {
     message: 'value, El número de caracteres mínimo es 3.',
   })
   @MaxLength(255, { message: 'value, Maximo 255 caracteres' })
