d7cf61d7b4e485c88b45946a82d0a57665611c66
Curipoma <at.curipoma@yavirac.edu.ec>
2022-10-01 00:42:14 -0500

    fix min length from DTO

--- src/modules/core/dto/catalogue-types/create-catalogue-types.dto.ts
+++ src/modules/core/dto/catalogue-types/create-catalogue-types.dto.ts
@@ -2,7 +2,7 @@ import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCatalogueTypesDto {
   @IsString({ message: 'name, Debe ser un string' })
-  @MinLength(3, { message: 'name, El número de caracteres mínimo es 3.' })
+  @MinLength(1, { message: 'name, El número de caracteres mínimo es 3.' })
   @MaxLength(255, { message: 'name, Maximo 255 caracteres' })
   readonly name: string;
 }
--- src/modules/core/dto/catalogues/create-catalogue.dto.ts
+++ src/modules/core/dto/catalogues/create-catalogue.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 import { CatalogueTypeEntity } from "@core/entities";
 
 export class CreateCatalogueDto {
@@ -6,7 +6,7 @@ export class CreateCatalogueDto {
   readonly type: CatalogueTypeEntity;
 
   @IsString({ message: 'name, Debe ser un string' })
-  @MinLength(3, { message: 'name, El número de caracteres mínimo es 3.' })
+  @MinLength(1, { message: 'name, El número de caracteres mínimo es 3.' })
   @MaxLength(255, { message: 'name, Maximo 255 caracteres' })
   readonly name: string;
 }
